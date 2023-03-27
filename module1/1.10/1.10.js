8eb7bb7c45f90924d0ea2e1a6dd8fc30d2a005b1
Jere <jerep.karppinen@gmail.com>
2023-03-27 22:09:15 +0300

    Module 1 done.

---
+++ module1/1.10/1.10.js
@@ -0,0 +1,18 @@
+const amountDices = parseInt(prompt('Enter a number of dice rolls: '));
+const sumEyes = parseInt(prompt('Sum of the eye numbers: '));
+const loops = 10000;
+let count = 0;
+
+for (let i = 0; i < loops; i++) {
+  let sum = 0;
+  for (let j = 0; j < amountDices; j++) {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    sum += dice;
+  }
+    if (sum === sumEyes) {
+      count++;
+    }
+  }
+
+const probability = count / loops * 100;
+document.querySelector('#target').innerHTML = `Probability to get sum ${sumEyes} with ${amountDices} dice is ${probability.toFixed(2)} %`;
--- module1/1.7/1.7.js
+++ module1/1.7/1.7.js
@@ -1,7 +1,7 @@
 const amount = parseInt(prompt('Enter a number of dice rolls: '));
 let sum = 0;
 
-for (let i = 1; i < amount; i++) {
+for (let i = 0; i < amount; i++) {
   let dice = Math.floor(Math.random() * 6) + 1;
   sum += dice;
 }
